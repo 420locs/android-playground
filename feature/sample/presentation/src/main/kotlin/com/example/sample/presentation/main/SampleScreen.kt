283fc3c03af576721b37758579a54b7a08e27307
420locs <ninhtbm@gmail.com>
2024-09-15 18:02:21 +0700

    remove unused imports

--- feature/sample/presentation/src/main/kotlin/com/example/sample/presentation/main/SampleScreen.kt
+++ feature/sample/presentation/src/main/kotlin/com/example/sample/presentation/main/SampleScreen.kt
@@ -1,29 +1,22 @@
 package com.example.sample.presentation.main
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.designSystem.design.MainToolbar
 import com.example.sample.domain.model.Post
 import com.example.sample.presentation.main.section.SampleScreenInitialSection
 import com.example.sample.presentation.main.section.SampleScreenInitialSectionState
-import com.example.sample.presentation.main.section.SampleScreenLoadingSection
-import com.example.sample.presentation.main.section.SampleScreenLoadingSectionState
 import com.example.sample.presentation.main.section.SampleScreenLoadedSection
 import com.example.sample.presentation.main.section.SampleScreenLoadedSectionState
+import com.example.sample.presentation.main.section.SampleScreenLoadingSection
+import com.example.sample.presentation.main.section.SampleScreenLoadingSectionState
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
