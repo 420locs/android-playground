5667528d6b80d2af8ae438e76600caadfd00f12f
420locs <ninhtbm@gmail.com>
2024-10-02 18:52:58 +0700

    Remove unnecessary permission request function

--- app/src/main/kotlin/com/example/trashArchitecture/MainActivity.kt
+++ app/src/main/kotlin/com/example/trashArchitecture/MainActivity.kt
@@ -2,12 +2,8 @@ package com.example.trashArchitecture
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.designSystem.theme.TrashArchitectureTheme
@@ -16,16 +12,13 @@ import com.example.home.presentation.navigation.home
 import com.example.media.MediaServiceConnection
 import com.example.sample.presentation.navigation.navigateToSample
 import com.example.sample.presentation.navigation.sample
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.isGranted
-import com.google.accompanist.permissions.rememberPermissionState
-import com.google.accompanist.permissions.shouldShowRationale
 import org.koin.android.ext.android.inject
 import org.koin.androidx.compose.KoinAndroidContext
 import org.koin.core.annotation.KoinExperimentalAPI
 
 class MainActivity : ComponentActivity() {
     private val mediaServiceConnection by inject<MediaServiceConnection>()
+
     @OptIn(KoinExperimentalAPI::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,32 +44,4 @@ class MainActivity : ComponentActivity() {
         mediaServiceConnection.destroyService()
     }
 
-    @OptIn(ExperimentalPermissionsApi::class)
-    @Composable
-    private fun RequestPermissions(
-        permission: String,
-        doOnPermissionGranted: () -> Unit,
-        doOnPermissionDenied: () -> Unit,
-        showRationale: (() -> Unit)? = null,
-    ) {
-        val cameraPermissionState =
-            rememberPermissionState(permission)
-        val requestPermissionLauncher = rememberLauncherForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted ->
-            if (isGranted) {
-                doOnPermissionGranted()
-            } else {
-                doOnPermissionDenied()
-            }
-        }
-
-        LaunchedEffect(cameraPermissionState) {
-            if (!cameraPermissionState.status.isGranted && cameraPermissionState.status.shouldShowRationale) {
-                showRationale?.invoke()
-            } else {
-                requestPermissionLauncher.launch(permission)
-            }
-        }
-    }
 }
