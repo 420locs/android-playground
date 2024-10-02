f5738b49c2bf80d5c1e78fbe855720653c19058a
420locs <ninhtbm@gmail.com>
2024-10-02 17:48:04 +0700

    Add ktor http client lib

--- bridge/src/main/java/com/example/bridge/core/DataModule.kt
+++ bridge/src/main/java/com/example/bridge/core/DataModule.kt
@@ -1,9 +1,17 @@
 package com.example.bridge.core
 
+import android.util.Log
 import com.example.network.NetworkApi
+import com.example.network.getKtorClient
 import com.example.sample.data.trash.PlaceHolderApi
+import io.ktor.client.HttpClient
 import org.koin.dsl.module
 
 internal val coreDataModules = module {
     single<NetworkApi> { PlaceHolderApi() }
+    single<HttpClient> {
+        getKtorClient{
+            Log.d("Ktor Log", it)
+        }
+    }
 }
