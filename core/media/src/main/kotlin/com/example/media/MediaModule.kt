d05e9e6ddbbe00917779b283d1d6d638ef40bebd
420locs <ninhtbm@gmail.com>
2024-10-03 22:19:54 +0700

    Try implement MediaSessionCallback: custom media action

--- core/media/src/main/kotlin/com/example/media/MediaModule.kt
+++ core/media/src/main/kotlin/com/example/media/MediaModule.kt
@@ -1,19 +1,29 @@
 package com.example.media
 
+import androidx.annotation.OptIn
 import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaSession
+import com.example.media.notification.MediaNotifier
+import com.example.media.notification.MediaSessionCallback
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 object MediaModule {
+    @OptIn(UnstableApi::class)
     operator fun invoke() = module {
+        singleOf(::MediaServiceConnection)
         single<Player> { ExoPlayer.Builder(get()).build() }
         single<MediaSession> {
+            val mediaSessionCallback = MediaSessionCallback(get())
             MediaSession.Builder(get(), get())
+                .setCallback(mediaSessionCallback)
+                .setCustomLayout(mediaSessionCallback.getCustomLayout())
                 .build()
         }
-        single<MediaNotifier> { MediaNotifier(get(), get()) }
-        single<MediaPlayerHandler> { MediaPlayerHandler(get()) }
-        single<MediaServiceConnection> { MediaServiceConnection(get()) }
+        singleOf(::MediaNotifier)
+        singleOf(::MediaPlayerHandler)
+        singleOf(::MusicPool)
     }
 }
