d4eb5d353702eeffc0b9c9715ff903f683fe6885
420locs <ninhtbm@gmail.com>
2024-10-06 20:33:40 +0700

    Reduce unused imports

--- bridge/src/main/java/com/example/bridge/HomeModule.kt
+++ bridge/src/main/java/com/example/bridge/HomeModule.kt
@@ -1,13 +1,10 @@
 package com.example.bridge
 
 import com.example.bridge.core.Module
-import com.example.home.data.GoogleSampleMusicApi
 import com.example.home.data.GoogleSampleMusicService
 import com.example.home.data.HomeInternalRepository
 import com.example.home.domain.HomeRepository
 import com.example.home.domain.useCase.GetAllRemoteSongs
-import com.example.network.NetworkApi
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 internal object HomeModule : Module() {
--- feature/home/data/src/main/kotlin/com/example/home/data/GoogleSampleMusicApi.kt
+++
@@ -1,7 +0,0 @@
-package com.example.home.data
-
-import com.example.network.NetworkApi
-
-class GoogleSampleMusicApi: NetworkApi() {
-    override val baseUrl: String = "https://storage.googleapis.com/uamp/catalog.json"
-}
